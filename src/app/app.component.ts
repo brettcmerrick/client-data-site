a79ddcbe8baee0bbe653b91297be7ed1ca840fd9
Brett Merrick <bmerrick@mastercontrol.com>
2020-04-23 13:35:56 -0600

    Changed Header on home page to be cleaner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,54 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'cm-root',
   template: `
-  <nav class='navbar navbar-expand navbar-light bg-light'>
-  <a class='navbar-brand'>{{title}}</a>
-  <ul class='navbar-nav'>
-    <li class='nav-item'><a class='nav-link' routerLinkActive='active'
-          [routerLink]="['/clientList']">Client List</a>
-    </li>
-  </ul>
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+          <img [src]="logoIcon"
+               [style.width.px]="logoWidth"
+               [style.margin.px]="logoMargin">
+        </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" routerLinkActive='active'
+          [routerLink]="['/clientList']">Client List
+                <span class="sr-only">(current)</span>
+              </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
 </nav>
-<div class='container'>
+
+<!-- Page Content -->
+<div class="container">
+  <h1 class="mt-4">{{title}}</h1>
   <router-outlet></router-outlet>
 </div>
+<!-- /.container -->
       
             `
 })
 export class AppComponent {
   title: string = 'Client Manager';
+  logoIcon = '../assets/images/logo.png';
+  logoWidth = '80';
+  logoMargin = '0';
+
+  
 }
